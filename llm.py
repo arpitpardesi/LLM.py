f5813ea8cfaf4c0e71979055291333a4e2bf0c7b
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-01-07 01:29:59 +0530

    initial llm script

---
+++ llm.py
@@ -0,0 +1,9 @@
+import ollama
+
+response = ollama.chat(
+    model='llama3.2',
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True
+)
+for chunk in response:
+    print(chunk['message']['content'], end='', flush=True)
