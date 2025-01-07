2dc370fe9eac1d7ac6464f489ce48391b6a4f60f
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-01-07 23:35:19 +0530

    continuous convo feature implemented

--- llm.py
+++ llm.py
@@ -1,9 +1,23 @@
 import ollama
 
-response = ollama.chat(
-    model='llama3.2',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
-    stream=True
-)
-for chunk in response:
-    print(chunk['message']['content'], end='', flush=True)
+convoHistory = []
+
+print("Cosmos: Hi I am your bot. How can I assist you today?", end='\n')
+while True:
+    bot = ''
+    quest = input("You: ")
+    convoHistory.append({'role': 'user', 'content': quest})
+    response = ollama.chat(
+        model='llama3.2',
+        messages=convoHistory,
+        stream=True
+    )
+    print("Cosmos: ", end='')
+    for chunk in response:
+        print(chunk['message']['content'], end='', flush=True)
+        bot += chunk['message']['content']
+    convoHistory.append({'role': 'assistant', 'content': bot})
+    print('\n')
+
+    if quest.lower() == 'exit':
+        break
