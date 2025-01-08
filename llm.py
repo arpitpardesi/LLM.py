7c5bc29c4041f039298e413fcde790c0577a5665
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-01-09 00:07:35 +0530

    Implementing Mondo DB for conversation storage

--- llm.py
+++ llm.py
@@ -1,6 +1,36 @@
 import ollama
+from pymongo import MongoClient
+import uuid
+from urllib.parse import quote_plus
+import certifi
 
+
+session_id = str(uuid.uuid4())
+
+
+MONGO_URI = f"mongodb+srv://{db_username}:{db_password}@cosmos.f2pie.mongodb.net/?retryWrites=true&w=majority&appName=Cosmos"  # Replace with your MongoDB connection URI
+client = MongoClient(MONGO_URI)
+db = client.cosmosBot
+conversation_collection = db.conversation_hist
+print(db, conversation_collection)
 convoHistory = []
+
+def load_conversation(session_id):
+    document = conversation_collection.find_one({"session_id": session_id})
+    if document:
+        return document["conversation"]
+    return []
+
+def save_conversation(session_id, conversation):
+    document = {
+        "session_id": session_id,
+        "conversation": conversation
+    }
+    conversation_collection.update_one(
+        {"session_id": session_id},
+        {"$set": document},
+        upsert=True
+    )
 
 print("Cosmos: Hi I am your bot. How can I assist you today?", end='\n')
 while True:
@@ -17,6 +47,8 @@ while True:
         print(chunk['message']['content'], end='', flush=True)
         bot += chunk['message']['content']
     convoHistory.append({'role': 'assistant', 'content': bot})
+    save_conversation(session_id, convoHistory)
+
     print('\n')
 
     if quest.lower() == 'exit' or quest.lower() == 'bye':
