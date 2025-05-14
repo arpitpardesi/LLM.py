678b873ea3ac625070a95824b216f0058b167b52
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-05-15 00:41:29 +0530

    script updated

--- Anaya/AnAIFriend.py
+++ Anaya/AnAIFriend.py
@@ -9,7 +9,7 @@ import certifi
 def load_conversation_history():
     try:
         history = list(conversation_collection.find().sort("timestamp", 1))
-        return [{"role": h["role"], "content": h["content"]} for h in history]
+        return [{"role": h["role"], "content": h["content"], "timestamp":h["timestamp"]} for h in history]
     except Exception as e:
         print("Error loading conversation history:", e)
         return []
@@ -17,7 +17,7 @@ def load_conversation_history():
 
 session_id = str(uuid.uuid4())
 
-MONGO_URI = f"mongodb+srv://{cred.db_username}:{cred.db_password}@cosmos.f2pie.mongodb.net/?retryWrites=true&w=majority&appName=Cosmos"  # Replace with your MongoDB connection URI
+MONGO_URI = f"mongodb+srv://{cred.db_username}:{cred.db_password}@cosmos.f2pie.mongodb.net/?retryWrites=true&w=majority&appName={cred.db_appname}"  # Replace with your MongoDB connection URI
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client.cosmosBot
 conversation_collection = db.anayaAI
@@ -59,7 +59,7 @@ def anayaResponse(convo, dialogID):
 
 
 if convoHistory[-1].get('role') == "assistant":
-    print(f"Anaya: {convoHistory[-1].get('content')}\n")
+    print(f"Anaya:  {convoHistory[-1].get('timestamp')}\n{convoHistory[-1].get('content')}\n")
 
 elif convoHistory[-1].get('role') == "user":
     anayaResponse(convo=convoHistory, dialogID=dialogID)
